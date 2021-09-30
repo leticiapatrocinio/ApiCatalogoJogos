daf0394ac9d0706175a41e0e501281be6ef329c9
Leticia Patrocinio <leticia_patrocinio@outlook.com>
2021-09-30 19:16:19 -0300

    Criação do projeto

---
+++ Services/IJogoService.cs
@@ -0,0 +1,20 @@
+﻿using ApiCatologoJogos.InputModel;
+using ApiCatologoJogos.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatologoJogos.Services
+{
+    public interface IJogoService : IDisposable
+    {
+        Task<List<JogoViewModel>> Obter(int pagina, int quantidade);
+        Task<JogoViewModel> Obter(Guid Id);
+        Task<JogoViewModel> Inserir(JogoInputModel jogo);
+        Task Atualizar(Guid id, JogoInputModel jogo);
+        Task Atualizar(Guid id, double preco);
+        Task Remover(Guid id);
+
+    }
+}
